1dfa9d98ca19bd26595d871fa225d42e239f8336
Bret Witt <legonxtboy@gmail.com>
2024-03-03 21:32:49 -1000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bretwitt.github.io',
+  base: '/bretwitt.github.io',
   integrations: [mdx(), sitemap(), tailwind()]
 });
